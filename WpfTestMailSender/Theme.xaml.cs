806806ce870da32c2ace19008cbd3fd4f82e3193
Kirill Polyakov <fieldoff@gmail.com>
2020-06-06 12:19:43 +0300

    Первый коммит

---
+++ WpfTestMailSender/Theme.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WpfTestMailSender
+{
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Theme : ContentView
+    {
+        public Theme()
+        {
+            //InitializeComponent();
+        }
+    }
+}
